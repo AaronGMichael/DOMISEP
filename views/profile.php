183655efc641f6ed7ab42b691dba8195bcf061be
pbonar <piotr.bonar@gmail.com>
2023-11-08 15:55:26 +0100

    changing profile page

--- views/profile.php
+++ views/profile.php
@@ -19,8 +19,28 @@ $row = $user->details($sql);
 <head>
 	<title>PHP Login using OOP Approach</title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DOMISEP - profile</title>
+    <link rel="stylesheet" type="text/css" href="../css/style-top-navigation.css">    
+    <link rel="stylesheet" type="text/css" href="../css/style-text.css">
+    <link rel="stylesheet" type="text/css" href="../css/style-background.css">
+    <link rel="stylesheet" type="text/css" href="../css/style-containers.css">
+    <link rel="stylesheet" type="text/css" href="../css/style-grid-layout.css">
+    <link rel="stylesheet" type="text/css" href="../css/style-buttons.css">
 </head>
 <body>
+	<div class="background-image"></div>
+        <header class="header">
+            <a class="logo"><titlesmaller>DomISEP</titlesmaller></a>
+            <ul class="navbar">
+                <a href="home.html">Home</a>
+                <a href="profile.html">Profile</a>
+                <a href="settings.html">Settings</a>
+                <a href="../utils/logout.php">Logout</a>
+                <a></a>
+            </ul>
+        </header>
 <div class="container">
 <?php
 		    	if(isset($_SESSION['messageLoggedIN'])){
@@ -33,17 +53,31 @@ $row = $user->details($sql);
 		    		unset($_SESSION['messageLoggedIN']);
 		    	}
 		    ?>
-	<h1 class="page-header text-center">PHP Login using OOP Approach</h1>
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4">
-			<h2>Welcome to Homepage </h2>
-			<h4>User Info: </h4>
-			<p>Name: <?php echo $row['fname']; ?></p>
-			<p>Username: <?php echo $row['username']; ?></p>
-			<p>Password: <?php echo $row['password']; ?></p>
-			<a href="../utils/logout.php" class="btn btn-danger"><span class="glyphicon glyphicon-log-out"></span> Logout</a>
-		</div>
+	<div class="additional-padding-big"></div>
+        <div class="basic-container">
+            <div class="additional-padding-little"></div>
+            <h1>Profile</h1>
+            <h2>
+                Here we can provide you with all the informations we store about you. You can change almost everything, 
+                besides name and surname. We hope that you will not get angry because of that...
+            </h2>
+                <sd>
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <label><?php echo $row['fname']; ?></label>
+                    </div>
+                    <div class="form-group">
+                        <label for="username">Username:</label>
+                        <label><?php echo $row['username']; ?></label>
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname">Password:</label>
+                        <label><?php echo $row['password']; ?></label>
+                    </div>
+                </sd>
+                <div class="additional-padding"></div>
+        </div>
+		<footer>Powered by <b>WebWizards</b></footer>
 	</div>
-</div>
 </body>
 </html>
