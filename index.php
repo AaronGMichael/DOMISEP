c9c19af52e8a53eabbe9c4078ffa4d187c3373c6
Aaron M <aaron.g.michael@gmail.com>
2023-11-08 15:33:48 +0100

    changed button underline on hover

--- index.php
+++ index.php
@@ -14,7 +14,10 @@
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>DOMISEP - Login</title>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="./css/style-login.css">
     <link rel="stylesheet" type="text/css" href="./css/style-text.css">
     <link rel="stylesheet" type="text/css" href="./css/style-containers.css">
@@ -63,8 +66,8 @@
                 <button type="submit" name="login" class="btn btn-lg btn-primary btn-block"><span class="glyphicon glyphicon-log-in"></span> Login</button>
 
             </fieldset>
-            <a type="button" class="button-unvisible-top" id="go-to-register" href="./views/register.php">You don't have an account? Register here</button>
-            <button type="button" class="button-unvisible" id="go-to-forgot-password">Forgot the password?</button>
+            <a type="button" class="button-unvisible-top no-underline-hover" href="./views/register.php">You don't have an account? Register here</a>
+            <button type="button" class="button-unvisible">Forgot the password?</button>
         </form>
 
         <footer>Powered by <b>WebWizards</b></footer>
