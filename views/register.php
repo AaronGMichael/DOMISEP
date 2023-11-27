33ed189812a64e533ea3203205f915d7a5feeb6b
pbonar <piotr.bonar@gmail.com>
2023-11-28 00:09:24 +0100

    changes in register page

--- views/register.php
+++ views/register.php
@@ -11,35 +11,66 @@
 <html>
 <head>
 	<title>PHP Login using OOP Approach</title>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>DOMISEP - Login</title>
+    <script 
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" 
+        crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" type="text/css" href="../css/style-login.css">
+    <link rel="stylesheet" type="text/css" href="../css/style-text.css">
+    <link rel="stylesheet" type="text/css" href="../css/style-containers.css">
+    <link rel="stylesheet" type="text/css" href="../css/style-buttons.css">
+    <link rel="stylesheet" type="text/css" href="../css/style-backgrounds.css">
 </head>
+<style>
+    a:hover {
+      text-decoration: none;
+    }
+</style>
 <body>
-<div class="container">
-	<h1 class="page-header text-center">PHP Register using OOP Approach</h1>
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4">
-		    <div class="login-panel panel panel-primary">
-		        <div class="panel-heading">
-		            <h3 class="panel-title"><span class="glyphicon glyphicon-lock"></span> Login
-		            </h3>
-		        </div>
-		    	<div class="panel-body">
-		        	<form method="POST" action="../utils/addUser.php">
-		            	<fieldset>
-		                	<div class="form-group">
-		                    	<input class="form-control" placeholder="Username" type="text" name="username" autofocus required>
-		                	</div>
-		                	<div class="form-group">
-		                    	<input class="form-control" placeholder="Password" type="password" name="password" required>
-		                	</div>
-							<div class="form-group">
-		                    	<input class="form-control" placeholder="Name" type="name" name="name" required>
-		                	</div>
-		                	<button type="submit" name="register" class="btn btn-lg btn-primary btn-block"><span class="glyphicon glyphicon-log-in"></span> Login</button>
-		            	</fieldset>
-		        	</form>
+	<div class="background-login-light"></div>
+        <section>
+            <div class="container mt-5 pt-5">
+                <div class="row">
+                    <div class="col-9 col-sm-7 col-md-6 m-auto">
+                        <div class="card border-0 shadow">
+                            <div class="card-body">
+                                <svg class="mx-auto my-3" xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="black" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                                    <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                                </svg>
+                                <h1 class="text-center">DomISEP</h1>
+								<form method="POST" action="../utils/addUser.php">
+									<fieldset>
+										<div class="form-group">
+											<label for="username">Username:</label>
+											<input class="form-control" placeholder="Username" type="text" name="username" autofocus required>
+										</div>
+										<div class="form-group">
+											<label for="password">Password:</label>
+
+											<input class="form-control" placeholder="Password" type="password" name="password" required>
+										</div>
+										<div class="form-group">
+											<label for="name">Name:</label>
+											<input class="form-control" placeholder="Name" type="name" name="name" required>
+										</div>
+										<button type="submit" name="register" class="button-submit btn btn-lg btn-primary btn-block"><span class="glyphicon glyphicon-log-in"></span> Register</button>
+									</fieldset>
+								</form>
+								<a type="button" class="button-unvisible" href="../index.php">You have an account? Login here</a>
+							</div>
+						</div>
+						<h6 class="text-white">© 2023 WebWizards</span>
+		    		</div>
 		    	</div>
-		    </div>
 		    <?php
 		    	if(isset($_SESSION['message'])){
 		    		?>
@@ -51,9 +82,9 @@
 		    		unset($_SESSION['message']);
 		    	}
 		    ?>
-		</div>
+			</div>
+		</section>
 	</div>
-</div>
-            </body>
-            </html>
+</body>
+</html>
             
