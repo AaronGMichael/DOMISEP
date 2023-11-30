3231c00a0588c7acb38979569837a800a5d7add9
Mahidhar09 <mahidhar75smile@gmail.com>
2023-11-30 17:16:56 +0100

    Added adddevicetype.phpand added button inroom.php

--- views/room.php
+++ views/room.php
@@ -75,6 +75,17 @@ include_once('../layout/header.php');
                                 <button type="submit" name="view-charts" class="button-submit">View Charts</button>
                             </div>
                         </div>
+                    </li>
+                    <li class="cards_item_grid">
+                        <div class="card_grid">
+                            <div class="card_content_grid">
+                                <div class="card_image_container_grid">
+                                    <a href="addsensor.php"><img src="../assets/add.png" style="width:150px;height:150px; margin-top:50px;"></a>
+                                    <h2 class="card_title_grid" style = "color: red;">Click To Add More!</h2>
+                                </div>
+
+                            </div>
+                        </div>
                     </li> 
 
                 </ul>
@@ -131,6 +142,17 @@ include_once('../layout/header.php');
                             </div>
                         </div>
                     </li>
+                    <li class="cards_item_grid">
+                        <div class="card_grid">
+                            <div class="card_content_grid">
+                                <div class="card_image_container_grid">
+                                    <a href="adddevicetype.php"><img src="../assets/add.png" style="width:150px;height:150px; margin-top:50px;"></a>
+                                    <h2 class="card_title_grid" style = "color: red;">Click To Add More!</h2>
+                                </div>
+
+                            </div>
+                        </div>
+                    </li> 
                 </ul>
                 <div class="additional-padding-small"></div>
 
