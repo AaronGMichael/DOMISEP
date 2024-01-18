af67a5ee336a69c7c302e380c2ba569902d2b67f
Basia1239 <przesadzalem@gmail.com>
2024-01-18 15:34:44 +0100

    updated sendhelp with js

--- views/sendHelp.php
+++ views/sendHelp.php
@@ -2,8 +2,9 @@
 include_once('../layout/header.php');
 ?>
 
+
 <div class="card border-0 shadow" style="margin:auto; width:80%; padding: 2em">
-<form action="../utils/help.php" method="post">
+<form action="../utils/help.php" method="post" onsubmit="return areYouSure()">
 <div class="container">
   <h1 style="margin-left: 0">How can we help you?</h1>
   <p style="margin-left: 1em">We will get back to you by email :)</p>
@@ -11,8 +12,16 @@ include_once('../layout/header.php');
       <input name="userid" value="<?php echo $user->id?>" style="display:none">
       <textarea name="message" placeholder="Your Message" class="form-control" rows="10" style="border: solid 2px black;" required></textarea>
     </div>
-    <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
-</div>
+    <button type="submit" class="btn btn-lg btn-dark btn-block" >Submit Form</button>
+<script>
+  function areYouSure(){
+    var txt;
+    const msg = document.querySelector("*[name='message']").value;
+    return confirm(`Are you sure you want to send this message?\n\n ${msg}`);
+  }
+  </script>
+
+  </div>
 </form>
 </div>
 
